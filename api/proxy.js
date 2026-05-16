bf06645dc2a8a7efb260e52870c3384d10de64e3
Boomedot <csgosalamander@gmail.com>
2026-05-16 21:14:30 +0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,27 +1,25 @@
-export const config = { runtime: "edge" };
+module.exports = async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "");
 
-export default async function handler(req) {
-  const { searchParams } = new URL(req.url);
-  const path = searchParams.get("path");
-  searchParams.delete("path");
+  if (req.method === "OPTIONS") return res.status(200).end();
 
-  const query = searchParams.toString();
-  const url = https://api-football-v1.p.rapidapi.com/v3/${path}${query ? "?" + query : ""};
+  try {
+    const { path, ...params } = req.query;
+    const query = new URLSearchParams(params).toString();
+    const url = https://api-football-v1.p.rapidapi.com/v3/${path}${query ? "?" + query : ""};
 
-  const response = await fetch(url, {
-    headers: {
-      "X-RapidAPI-Key": "fcd8d80d34msh5262affbb668a07p1760a3jsne36b5e920dfb",
-      "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
-    },
-  });
+    const response = await fetch(url, {
+      headers: {
+        "X-RapidAPI-Key": "fcd8d80d34msh5262affbb668a07p1760a3jsne36b5e920dfb",
+        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+      },
+    });
 
-  const data = await response.json();
-
-  return new Response(JSON.stringify(data), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
-}
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
