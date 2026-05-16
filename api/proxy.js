e156a7303f6a6dcfc9b7711700414c60f1231fc9
Boomedot <csgosalamander@gmail.com>
2026-05-16 20:15:09 +0300

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") return res.status(200).end();
+
+  const { path, ...params } = req.query;
+  const query = new URLSearchParams(params).toString();
+  const url = https://api-football-v1.p.rapidapi.com/v3/${path}${query ? "?" + query : ""};
+
+  const response = await fetch(url, {
+    headers: {
+      "X-RapidAPI-Key": "fcd8d80d34msh5262affbb668a07p1760a3jsne36b5e920dfb",
+      "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+    },
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
